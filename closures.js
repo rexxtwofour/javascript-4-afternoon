1d137dcb2cebf3ea71c55576dd9d7de72e96b683
Eric Rivera <ericrivera@Eric-Riveras-MacBook-Pro.local>
2018-07-12 20:51:21 -0700

    initial commit

--- closures.js
+++ closures.js
@@ -21,9 +21,9 @@ function outer() {
   Above you're given a function that returns another function which has a closure over the name variable.
   Invoke outer saving the return value into another variable called 'inner'.
 */
-  
-// Code Here
 
+// Code Here
+const inner = outer()
 
 
 //Once you do that, invoke inner.
@@ -36,13 +36,12 @@ function outer() {
 
 // Do not edit the code below.
 function callFriend(name) {
-  function dial(number) {
-    return 'Calling ' + name + ' at ' + number
+  function dial(valueber) {
+    return 'Calling ' + name + ' at ' + valueber
   }
   return dial;
 }
 // Do not edit the code above.
-
 /*
   Above you're given a callFriend function that returns the dial function.
   Store the result of invoking callFriend in a variable named callJake.
@@ -51,7 +50,10 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
+
+const callJake = callFriend('Jake')
 //Code Here
+callJake('435-555-9248')
 
 
 
@@ -63,14 +65,21 @@ function callFriend(name) {
 
 //Code Here
 
+function makeCounter(){
+  let value = 0 
+    return function(){
+      value++
+      return value
+    }
+}
 
 
-//Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+// // Uncomment this once you make your function
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -89,15 +98,25 @@ function counterFactory(value) {
   // Code here.
 
   return {
+    inc: function(){
+      value++
+      return value
 
+    },
+    dec: function(){
+      value--
+      return value
+
+
+    }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -112,10 +131,14 @@ counter = counterFactory(10);
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
+  function message(){
+    return welcomeText+' '+ firstname +' '+ lastname +'.';
+  }
+
   // code message function here.
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message; 
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -143,7 +166,10 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod(){
+      return privateMethod()
+        
+    }
   };
 })();
 
@@ -152,16 +178,26 @@ var module = (function() {
 ////////// PROBLEM 7 //////////
 
 /*
-  Here we have a function named secretNumber that has a secret number.
+  Here we have a function named secretvalueber that has a secret valueber.
   Inside the return object, create two methods called addToSecret and takeAwayFromSecret.
-  addToSecret should have a parameter that is added to the secret number returning the updated secret number.
-  takeAwayFromSecret should have a parameter that takes away from the secret number returning the updated secret number.
+  addToSecret should have a parameter that is added to the secret valueber returning the updated secret valueber.
+  takeAwayFromSecret should have a parameter that takes away from the secret valueber returning the updated secret valueber.
 */
 
 function secretNumber() {
   var secret = 143;
 
   return {
+
+    addToSecret(num){
+      num += secret
+      return secret
+    },
+    takeAwayFromSecret(num){
+      num -= secret
+      return secret
+    }
+
     // Code here
   };
 }
